a484db30b55ec553234d6e6bbc872c481d687a94
ZAGROX <37931784+zagrox@users.noreply.github.com>
2025-05-06 12:19:38 +0330

    Add password reset language files

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Su contraseña ha sido restablecida.',
+    'sent' => 'Hemos enviado por correo electrónico el enlace para restablecer su contraseña.',
+    'throttled' => 'Por favor espere antes de intentar de nuevo.',
+    'token' => 'Este token de restablecimiento de contraseña es inválido.',
+    'user' => 'No podemos encontrar un usuario con esa dirección de correo electrónico.',
+]; 
